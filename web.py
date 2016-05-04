a38a7bfc2dd58dd86bc04f4ff4a8193b12577bcc
skullY <skullydazed@gmail.com>
2016-05-03 22:27:42 -0700

    Add the ability to save and load layouts

--- web.py
+++ web.py
@@ -1,8 +1,9 @@
 import json
 from glob import glob
 from os.path import exists
+from pprint import pprint
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
@@ -31,6 +32,7 @@ def render_page(page_name, **args):
         'len': len,
         'sorted': sorted,
         'str': str,
+        'type': type,
         'zip': zip
     }
     arguments.update(args)
@@ -41,6 +43,11 @@ def render_page(page_name, **args):
     return render_template('%s.html' % page_name, **arguments)
 
 
+@app.route('/')
+def index():
+    return redirect('/keyboard/' + app.config['DEFAULT_KEYBOARD'])
+
+
 @app.route('/keyboard/<keyboard>')
 def keyboard(keyboard):
     if '..' in keyboard or '/' in keyboard:
@@ -49,36 +56,15 @@ def keyboard(keyboard):
     if not exists(keyboard + '_layout.json'):
         return render_page('error', error='No such keyboard!')
 
-    layers_layout = json.load(open(keyboard + '_layout.json'))
-    keyboard_properties = layers_layout.pop(0)
-    pcb_layout = layers_layout.pop(0)
+    layers = json.load(open(keyboard + '_layout.json'))
 
-    layers = []
-    pcb_rows = []
-    key_attr = {}
-    layouts = [(pcb_layout, pcb_rows)]
+    return build_layout_page(layers)
 
-    for layer in layers_layout:
-        layers.append([])
-        layouts.append((layer, layers[-1]))
 
-    for layout, rows in layouts:
-        for row in layout:
-            rows.append([])
-            if isinstance(row, list):
-                for key in row:
-                    if isinstance(key, dict):
-                        for k, v in key.items():
-                            key_attr[k] = v
-                    else:
-                        if 'w' not in key_attr:
-                            key_attr['w'] = 1
-                        if 'h' not in key_attr:
-                            key_attr['h'] = 1
-
-                        key_attr['name'] = key
-                        rows[-1].append(key_attr)
-                        key_attr = {}
+def build_layout_page(layers):
+    keyboard_properties = layers.pop(0)
+    pcb_rows = layers.pop(0)
+    keyboard_properties['key_width'] = int(keyboard_properties['key_width'])
 
     return render_page('index',
                        title=keyboard_properties['name'],
@@ -88,9 +74,34 @@ def keyboard(keyboard):
                        pcb_rows=pcb_rows)
 
 
-@app.route('/')
-def index():
-    return redirect('/keyboard/' + app.config['DEFAULT_KEYBOARD'])
+@app.route('/save', methods=['POST'])
+def POST_save():
+    """Enable downloading the saved keyboard layout.
+    """
+    filename = request.form.get('filename')
+    json_data = request.form.get('json_data')
+    response = Response(json_data)
+    response.headers['Content-Disposition'] = 'attachment; filename=%s.json' % filename
+    response.headers['Content-Type'] = 'text/json'
+    response.headers['Pragma'] = 'no-cache'
+
+    return response
+
+
+@app.route('/load', methods=['POST'])
+def POST_load():
+    """Enable uploading and editing the saved keyboard layout.
+    """
+    print request.files
+
+    file = request.files['json_file']
+    extension = file.filename.rsplit('.', 1)[1]
+
+    if extension != 'json':
+        return render_page('error', error='File must be JSON!')
+
+    layers = json.load(file.stream)
+    return build_layout_page(layers)
 
 
 if __name__ == '__main__':
